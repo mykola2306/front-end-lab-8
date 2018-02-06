a1379fb6c4415f821a924523861d19377efbf533
mykola2306 <mykolag21@gmail.com>
2018-02-06 22:00:21 +0200

    H6 Added isosceles triangle detection

--- homework_06/js/task1.js
+++ homework_06/js/task1.js
@@ -3,7 +3,7 @@ function typeAndSquare() {
   let square = 0;
   let sideA = 4.52;
   let sideB = 4.52;
-  let sideC = 4.52;
+  let sideC = 5;
 
   if ((sideA * sideA + sideB * sideB) == sideC * sideC) {
     phrase += "right triangle ";
@@ -15,6 +15,14 @@ function typeAndSquare() {
   if (sideA == sideB & sideB == sideC) {
     phrase += "equilateral triangle ";
     square = ((sideA * sideA * Math.sqrt(3)) / 4).toFixed(2);
+    phrase += "and square is " + square;
+    console.log(phrase);
+  }
+
+  if (sideA === sideB || sideA === sideC || sideB === sideC) {
+    phrase += "isosceles triangle ";
+    let s = (sideA + sideB + sideC) / 2;
+    square = (Math.sqrt(s*(s-sideA)*(s-sideB)*(s-sideC))).toFixed(2);
     phrase += "and square is " + square;
     console.log(phrase);
   }
