854ce0f36c7b68368ad1f63c2460d8a2707c13ee
mykola2306 <mykolag21@gmail.com>
2018-02-13 22:19:45 +0200

    H8 Added getMin function

---
+++ homework_08/js/getMin.js
@@ -0,0 +1,10 @@
+function getMin(){
+  let minNum = arguments[0]
+  for(let i = 1; i < arguments.length; i++){
+    if(arguments[i] < minNum){
+      minNum = arguments[i];
+    }
+  }
+  return minNum;
+}
+getMin(1,2,5,-1,4);
