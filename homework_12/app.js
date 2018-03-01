4ecdabded912b0d5cf0752d30355c864098aa7ea
mykola2306 <mykolag21@gmail.com>
2018-03-01 23:13:10 +0200

    H12 app.js small code clean up
    Remove all console logs
    Remove unnecessary commented code

--- homework_12/app.js
+++ homework_12/app.js
@@ -1,12 +1,12 @@
 location.hash = "";
-// Your code goes here
+
 let rootNode = document.getElementById("root");
 let pageHeading = document.createElement('h1');
 let page = document.createElement('div')
 let tanksList = document.createElement('div');
 
 function tankPreviewPage(arrTankObj) {
-  pageHeading.innerHTML = `Most popular tanks`;  
+  pageHeading.innerHTML = `Most popular tanks`;
   page.classList.add("thumbnails");
   page.appendChild(pageHeading);
   tanksList.classList.add('tanksList');
@@ -22,46 +22,25 @@ function tankPreviewPage(arrTankObj) {
     tankName.classList.add("tooltipImg")
     tankFlag.classList.add("tooltipImg")
     if (arrTankObj[i].model) {
-      console.log('model: ', arrTankObj[i].model);
       tankItemLink.setAttribute('href', `#${arrTankObj[i].model}`.replace(/\s+/g, '-').toLowerCase());
       tankImg.setAttribute("src", arrTankObj[i].preview);
       tankImg.classList.add("imgBlock");
-      // tankFlag.innerHTML = `heloo`
-      // tankName.innerHTML = `atext`
-      tankFlag.innerHTML = `
-        <span class="tooltipImgText">${arrTankObj[i].country}</span>
-        <img alt="${arrTankObj[i].country}" class="imgAutoWidth" src="${arrTankObj[i].country_image}">`;
-      tankName.innerHTML = `
-      <span class="tooltipName">${arrTankObj[i].model}</span>
-      <span>${arrTankObj[i].level}</span> ${arrTankObj[i].model}`;
 
-      // tankItem.innerHTML = arrTankObj[i].model;
+      tankFlag.innerHTML = `<span class="tooltipImgText">${arrTankObj[i].country}</span><img alt="${arrTankObj[i].country}" class="imgAutoWidth" src="${arrTankObj[i].country_image}">`;
+      tankName.innerHTML = `<span class="tooltipName">${arrTankObj[i].model}</span><span>${arrTankObj[i].level}</span> ${arrTankObj[i].model}`;
+
       tankItem.appendChild(tankImg);
       tankItem.appendChild(tankFlag);
       tankItem.appendChild(tankName);
       tankItemLink.appendChild(tankItem);
       tanksList.appendChild(tankItemLink);
-
-      // li.innerHTML = `<p><i class="material-icons orange">folder</i> ${arrTankObj[i].title}</p>`;
-      // li.firstChild.classList.add('folder');
-      if (arrTankObj[i].children) {
-        // let childUl = tankPreviewPage(arrTankObj[i].children);
-        // li.appendChild(childUl);
-      } else {
-        // li.innerHTML += "<ul><i>Folder is empty</i></ul>";
-      }
-    } else {
-      // li.innerHTML = `<p><i class="material-icons grey">insert_drive_file</i> ${arrTankObj[i].title}</p>`;
     }
-    // ul.appendChild(li);
   }
   page.appendChild(tanksList);
   return page;
 }
 
 function tankDetails(tank) {
-  console.log(tank);
-  console.log(tank.model);
   page.classList.remove("thumbnails");
   page.classList.add("tank-details");
   document.querySelector(".tanksList").innerHTML = ``;
@@ -74,17 +53,15 @@ function tankDetails(tank) {
   detailsTable.innerHTML = `<h2>Characteristic</h2>`;
   detailsTable.appendChild(buildTable(tank.details));
   let returnToThumbnails = document.createElement('div');
-  returnToThumbnails.innerHTML = `<a aria-label="services for cars" href="#">Back to list view</a>`;
+  returnToThumbnails.innerHTML = `<a href="#">Back to list view</a>`;
   tanksList.appendChild(detailsImg);
   tanksList.appendChild(detailsTable);
   tanksList.appendChild(returnToThumbnails);
 }
 
 function buildTable(table) {
-  console.log(table);
   let tableBuild = document.createElement('table');
   for (var key in table) {
-    // console.log(key + " -> " + table[key]);
     let row = document.createElement('tr');
     let tdName = document.createElement('td');
     tdName.innerText = key.replace(/_/g, ' ');;
@@ -98,21 +75,18 @@ function buildTable(table) {
 }
 
 rootNode.appendChild(tankPreviewPage(tanks));
-// tankDetails(tanks[2]);
+
 window.addEventListener("hashchange", function () {
-  console.log('The anchor part has changed!');
   for (let i = 0; i < tanks.length; i++) {
     if (location.hash === `#${tanks[i].model}`.replace(/\s+/g, '-').toLowerCase()) {
       tankDetails(tanks[i]);
       return;
     }
   }
-  if(location.hash = "#"){
-    console.log("YES CHANGE!!!!");
+  if (location.hash = "#") {
     tanksList.innerHTML = "";
     page.classList.remove("tank-details");
     page.classList.add("thumbnails");
     rootNode.appendChild(tankPreviewPage(tanks));
   }
 });
-
